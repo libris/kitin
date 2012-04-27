c270ffd13eec44e3b27c971cf0884cc0ab00a06f
Markus Sköld <markus.skold@kb.se>
2012-04-27 15:09:15 +0200

    One fabfile to rule them all

--- fabfile.py
+++ fabfile.py
@@ -9,4 +9,6 @@ def create_db():
     db = create_engine('sqlite:///kitin2.db')
 
 def prepare():
+#    with prefix('workon kitin'):
+#        local('pip install -r dev-requirements.txt')
     create_db()
