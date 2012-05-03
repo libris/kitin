6703f24b07270d892e78a294f8d1f79f72960345
Markus Sköld <markus.skold@kb.se>
2012-05-03 14:55:24 +0200

    Added fab task to create wsgi-file

--- fabfile.py
+++ fabfile.py
@@ -23,6 +23,13 @@ def create_db():
         )
         marcpost.create()
 
+def create_wsgi_file():
+    wsgifile = open('kitin.wsgi', 'w')
+    wsgifile.write("activate_this = '%s/kitin/bin/activate_this.py'\n" % os.environ.get('WORKON_HOME'))
+    wsgifile.write("execfile(activate_this, dict(__file__=activate_this))\n\n")
+    wsgifile.write("from kitin import app as application\n")
+    wsgifile.close()
+
 def prepare():
     if not os.path.exists(appconfig['UPLOAD_FOLDER']):
         os.mkdir(appconfig['UPLOAD_FOLDER'])
