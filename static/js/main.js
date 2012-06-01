de5d791ed0131745ee533c4e2d86eeae9fb1f650
Jens Tinfors <jens@tinfors.se>
2012-06-01 11:02:31 +0200

    Adds Autogrow and better follow twitter bootstrap design

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,65 @@
+(function($){
+  // jQuery autoGrowInput plugin by James Padolsey
+  // See related thread: http://stackoverflow.com/questions/931207/is-there-a-jquery-autogrow-plugin-for-text-fields
+  $.fn.autoGrowInput = function(o) {
+
+    o = $.extend({
+      maxWidth: 1000,
+      minWidth: 0,
+      comfortZone: 70
+    }, o);
+
+    this.filter('input:text').each(function(){
+
+      var minWidth = o.minWidth || $(this).width(),
+      val = '',
+      input = $(this),
+      testSubject = $('<tester/>').css({
+        position: 'absolute',
+        top: -9999,
+        left: -9999,
+        width: 'auto',
+        fontSize: input.css('fontSize'),
+        fontFamily: input.css('fontFamily'),
+        fontWeight: input.css('fontWeight'),
+        letterSpacing: input.css('letterSpacing'),
+        whiteSpace: 'nowrap'
+      }),
+      check = function() {
+
+        if (val === (val = input.val())) {return;}
+
+        // Enter new content into testSubject
+        var escaped = val.replace(/&/g, '&amp;').replace(/\s/g,'&nbsp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+        testSubject.html(escaped);
+
+        // Calculate new width + whether to change
+        var testerWidth = testSubject.width(),
+        newWidth = (testerWidth + o.comfortZone) >= minWidth ? testerWidth + o.comfortZone : minWidth,
+        currentWidth = input.width(),
+        isValidWidthChange = (newWidth < currentWidth && newWidth >= minWidth)
+        || (newWidth > minWidth && newWidth < o.maxWidth);
+
+        // Animate width
+        if (isValidWidthChange) {
+          input.width(newWidth);
+        }
+
+      };
+
+      testSubject.insertAfter(input);
+
+      $(this).bind('keyup keydown blur update', check);
+
+      check();
+    });
+
+    return this;
+
+  };
+
+})(jQuery);
+//
 //record
 // - uid
 // - marc
@@ -45,6 +107,7 @@ $.fn.serializeObject = function() {
 }
 
 $(function() {
+
   this.collection = new Collection();
   this.router = new Router([this.collection]);
 
@@ -62,7 +125,6 @@ $(function() {
   // TODO: onunload:
   //if (ajaxInProgress)
   //  confirm('ajaxInProgress; break and leave?')
-
 });
 
 var Record = Backbone.Model.extend({
@@ -157,6 +219,7 @@ var View = Backbone.View.extend({
 
     this.setupBibAutocomplete();
     this.setupRecordKeyBindings();
+    $('.subfield').autoGrowInput({comfortZone: 20, minWidth: 60, maxWidth: 140});
 
   },
 
@@ -244,3 +307,4 @@ var MARC = {
   }
 
 }
+
