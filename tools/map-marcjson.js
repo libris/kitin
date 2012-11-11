0a426643a210d7276a81762b0173cb77c92e2381
Niklas Lindström <lindstream@gmail.com>
2012-11-11 18:49:26 +0100

    Add util funcs and initial createEditMap impl to marcjson

--- tools/map-marcjson.js
+++ tools/map-marcjson.js
@@ -9,13 +9,23 @@ function loadJson(path) {
 var marcMapPath = process.argv[2];
 var recordType = process.argv[3];
 var marcStructPath = process.argv[4];
+var op = process.argv[5];
 
 var marcmap = loadJson(marcMapPath)
 if (recordType)
   marcmap = marcmap[recordType];
 var struct = loadJson(marcStructPath);
 
-var namedStruct = marcjson.rawToNamed(marcmap, struct);
+var out;
+if (op === '-n') {
+  out = marcjson.rawToNamed(marcmap, struct);
+} else if (op !== undefined) {
+  var overlay = loadJson(op);
+  out = marcjson.createEditMap(marcmap, overlay, struct);
+} else {
+  marcjson.expandFixedFields(marcmap, struct);
+  out = struct;
+}
 
-console.log(JSON.stringify(namedStruct, null, 2));
+console.log(JSON.stringify(out, null, 2));
 
