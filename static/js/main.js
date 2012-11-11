57337bbf49b1dd413b64bc6b50f0526f401eb95d
Niklas Lindström <lindstream@gmail.com>
2012-11-11 18:59:07 +0100

    Add initial toggle and rendering of future mode

--- static/js/main.js
+++ static/js/main.js
@@ -103,11 +103,6 @@ function RecordCtrl($scope, $location, $http, $timeout) {
           (indEnum['_'].id === 'undefined' ||
             indEnum['_'].label_sv === 'odefinierad')) {
         return 'hidden';
-      // TODO: hack before overlay field config is in place
-      } else if (tag + indKey == '245ind1') {
-        return 'boolean';
-      } else if (tag + indKey == '245ind2') {
-        return 'number';
       } else if (indEnum) {
         return 'select';
       } else {
