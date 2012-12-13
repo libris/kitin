3c4b13b02a264553670170bd1dbeca72210fdbb4
Niklas Lindström <lindstream@gmail.com>
2012-12-13 22:15:25 +0100

    Extract and use decorateMarcFieldDefinition for extending metadata when creating groups

--- static/js/marcjson.js
+++ static/js/marcjson.js
@@ -238,7 +238,12 @@ var marcjson = typeof exports !== 'undefined'? exports : {};
     var targetGroup = [];
     var defs = targetGroup.fieldDefs = [];
     groupSpec.forEach(function (tag) {
-      if (typeof tag === 'string') defs.push(map[tag]);
+      if (typeof tag === 'string') {
+        decorateMarcFieldDefinition(map, overlay, tag);
+        defs.push(map[tag]);
+      } else {
+        decorateMarcFieldDefinition(map, overlay, exports.getMapEntryKey(tag));
+      }
     });
     targetGroup.addField = function (tag) {
       var newField = exports.addField(struct, tag, map[tag]);
@@ -256,8 +261,7 @@ var marcjson = typeof exports !== 'undefined'? exports : {};
     return targetGroup;
   }
 
-  function decorateMarcField(map, overlay, tag, field) {
-    // TODO: prepare map data: clone dfn once, extend with overlay
+  function decorateMarcFieldDefinition(map, overlay, tag) {
     var dfn = map[tag];
     dfn.tag = tag;
     var tagExt = overlay.extend[tag];
@@ -293,6 +297,10 @@ var marcjson = typeof exports !== 'undefined'? exports : {};
     //  subCode = exports.getMapEntryKey(subfield);
     //  return this.subfield[subCode];
     //};
+  }
+
+  function decorateMarcField(map, overlay, tag, field) {
+    var dfn = map[tag];
     field.getRow = function () { return this[tag]; };
     field.getTagDfn = function () { return dfn; };
     field.getWidgetType = function () {
