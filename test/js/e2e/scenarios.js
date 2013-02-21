4ec3b1eac9a42bb7e9a370f23b4210a5081288df
Jens Tinfors <jens.tinfors@gmail.com>
2013-02-21 12:22:43 +0100

    Adds a skeleton setup for Angularjs end2end tests

---
+++ test/js/e2e/scenarios.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
+
+describe('Kitin App', function() {
+
+  describe('Search view', function() {
+
+    beforeEach(function() {
+      browser().navigateTo('/search');
+    });
+
+
+    it('should have a search bar', function() {
+      expect(input('q').attr('type')).toEqual("text");
+    });
+  });
+});
