4a93c417a9df0202340bd79ea1388633a5b97497
Jens Tinfors <jens.tinfors@gmail.com>
2013-02-26 15:32:00 +0100

    Tidying up the storage_test after review with @niklasl

--- test/python/test_storage.py
+++ test/python/test_storage.py
@@ -1,11 +1,13 @@
 import os, shutil, json
 from storage import Storage
 
+basedir = os.path.dirname(__file__) + "/"
+
 def test_save_json():
     """Verify that we can open the saved file and extract some data"""
     storage = Storage("some/path")
-    org_file = open("fixture/7149593_formatted.json", "r")
-    storage.save("bib", "7149593", json.loads(org_file.read()))
+    with open(basedir + "fixture/7149593_formatted.json", "r") as f:
+        storage.save("bib", "7149593", json.loads(f.read()))
     with open("some/path/bib/7149593", "r") as f:
         id_of_data = json.loads(f.read())['@id'].rsplit("/",1)[1]
         assert  id_of_data == '7149593'
@@ -15,8 +17,7 @@ def test_update_json():
     """Saves the json and then updates it with a new value.
     Verifies that we can read back the new value from file"""
     storage = Storage("some/path")
-    org_file = open("fixture/7149593_formatted.json", "r")
-    with open("fixture/7149593_formatted.json", "r") as f:
+    with open(basedir + "fixture/7149593_formatted.json", "r") as f:
         data = f.read()
         storage.save("bib", "7149593", json.loads(data))
         json_data = json.loads(data)
@@ -27,15 +28,15 @@ def test_update_json():
 
 def test_delete_json():
     storage = Storage("some/path")
-    org_file = open("fixture/7149593_formatted.json", "r")
-    json_data = json.loads(org_file.read())
+    with open(basedir + "fixture/7149593_formatted.json", "r") as f:
+        json_data = json.loads(f.read())
     storage.save("bib", "7149593", json_data)
     storage.delete("bib", "7149593")
     shutil.rmtree("some")
 
 def test_read_json():
     storage = Storage("some/path")
-    with open("fixture/7149593_formatted.json", "r") as f:
+    with open(basedir + "fixture/7149593_formatted.json", "r") as f:
         data = f.read()
         storage.save("bib", "7149593", json.loads(data))
         json_data = json.loads(data)
