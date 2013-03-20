9b74a8a37a2fb0ddb72f27dfcd3268a43d26f7b4
Jens Tinfors <jens.tinfors@gmail.com>
2013-03-20 11:22:42 +0100

    Adds functionality to add more holdings to list. POSTs to whelk if new, otherwise PUTs

--- kitin.py
+++ kitin.py
@@ -97,6 +97,24 @@ def get_holding(holding_id):
     else:
         abort(response.status_code)
 
+@app.route('/holding', methods=['POST'])
+def create_holding():
+    path = "%s/hold/" % (app.config['WHELK_HOST'])
+    response = requests.post(path, data=request.data, allow_redirects=False)
+    if response.status_code == 200:
+        resp = raw_json_response(response.text)
+        resp.headers['etag'] = response.headers['etag'].replace('"', '')
+    elif response.status_code == 303:
+        data = {}
+        data['document'] = json.loads(response.text)
+        data['document_id'] = urlparse(response.headers['Location']).path.rsplit("/")[-1]
+        resp = raw_json_response(json.dumps(data))
+        resp.headers['etag'] = response.headers['etag'].replace('"', '')
+        return resp
+    else:
+        abort(response.status_code)
+
+
 @app.route('/holding/<holding_id>', methods=['PUT'])
 def save_holding(holding_id):
     if_match = request.headers['If-match']
--- static/js/main.js
+++ static/js/main.js
@@ -206,13 +206,24 @@ function FrbrCtrl($scope, $http, $routeParams, $timeout, records, resources, con
 
   $scope.save_holding = function(holding) {
     var etag = $scope.holding_etags[holding['@id']];
-    console.log("holding etag: " + etag);
-    $http.put("/holding/" + holding['@id'].split("/").slice(-2)[1], holding, {headers: {"If-match":etag}}).success(function(data, status, headers) {
-      console.log("successfully saved holding with id: " + holding['@id']);
-      $scope.holding_etags[data['@id']] = headers('etag');
-    }).error(function(data, status, headers) {
-      console.log("ohh crap!");
-    });
+    if(etag != undefined) {
+      console.log("holding etag: " + etag);
+      $http.put("/holding/" + holding['@id'].split("/").slice(-2)[1], holding, {headers: {"If-match":etag}}).success(function(data, status, headers) {
+        console.log("successfully saved holding with id: " + holding['@id']);
+        $scope.holding_etags[data['@id']] = headers('etag');
+      }).error(function(data, status, headers) {
+        console.log("ohh crap!");
+      });
+    } else {
+      $http.post("/holding", holding).success(function(data, status, headers) {
+        console.log("successfully saved holding with id: " + holding['@id']);
+        $scope.holding_etags[data['@id']] = headers('etag');
+        $scope.holdings.push(data['document']);
+      }).error(function(data, status, headers) {
+        console.log("ohh crap!");
+      });
+
+    }
   }
 
   $scope.delete_holding = function(index) {
@@ -250,6 +261,10 @@ function FrbrCtrl($scope, $http, $routeParams, $timeout, records, resources, con
   }).error(function(data, status) {
     console.log(status);
   });
+
+  $scope.add_holding = function(holdings) {
+    holdings.push({shelvingControlNumber: "", location: constants.get("user_sigel")});
+  }
 
   $scope.add_person = function(authors) {
     authors.push({ authoritativeName: "", birthYear: "" });
