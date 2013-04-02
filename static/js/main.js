debacc497abb2c8922a073b7367bd9a58ca0e12f
Per Åberg <per.aberg@kb.se>
2013-04-02 17:10:11 +0200

    Begun textarea stuff

--- static/js/main.js
+++ static/js/main.js
@@ -204,8 +204,11 @@ function SearchCtrl($scope, $http, $location, $routeParams) {
   }
   
   // Bread Crumbs
+  //$scope.crumbs = [{"term" : "books", "urlpart" : "f=books"}, {"term:" : "year", "urlpart" : "f=2004"}];
+  
   tmp_crumbs = previous_facets.split(" ");
   var aggr_crumbs = [];
+  console.log("LLLLL: ", tmp_crumbs);
   if (tmp_crumbs.length > 0) {
      aggr_crumbs[0] = tmp_crumbs[0];
      for (i=1; i < tmp_crumbs.length; i++) {
@@ -638,6 +641,48 @@ kitin.directive('keyEsc', function () {
   }
 });
 
+/*kitin.directive('autoGrow', function() {
+return function(scope, element, attr){
+var minHeight = element[0].offsetHeight,
+paddingLeft = element.css('paddingLeft'),
+paddingRight = element.css('paddingRight');
+ 
+var $shadow = angular.element('<div></div>').css({
+position: 'absolute',
+top: -10000,
+left: -10000,
+width: element[0].offsetWidth - parseInt(paddingLeft ? paddingLeft : 0) - parseInt(paddingRight ? paddingRight : 0),
+fontSize: element.css('fontSize'),
+fontFamily: element.css('fontFamily'),
+lineHeight: element.css('lineHeight'),
+resize: 'none'
+});
+angular.element(document.body).append($shadow);
+ 
+var update = function() {
+var times = function(string, number) {
+    console.log("STRING: " + string + " Number: " + number);
+for (var i = 0, r = ''; i < number; i++) {
+r += string;
+}
+return r;
+}
+ 
+var val = element.val().replace(/</g, '&lt;')
+.replace(/>/g, '&gt;')
+.replace(/&/g, '&amp;')
+.replace(/\n$/, '<br/>&nbsp;')
+.replace(/\n/g, '<br/>')
+.replace(/\s{2,}/g, function(space) { return times('&nbsp;', space.length - 1) + ' ' });
+$shadow.html(val);
+ 
+element.css('height', Math.max($shadow[0].offsetHeight + 10, minHeight) + 'px');
+}
+ 
+element.bind('keyup keydown keypress change', update);
+update();
+}
+});*/
 
 kitin.directive('fadable', function(conf) {
   return function(scope, elm, attrs) {
