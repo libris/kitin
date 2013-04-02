4a6898e21d010c7fe54f1744020287cdf3ff2dd5
Per Åberg <per.aberg@kb.se>
2013-04-02 17:54:19 +0200

    Introducing angular ui for textarea autosize etc

--- static/js/main.js
+++ static/js/main.js
@@ -11,7 +11,7 @@ function getParameterByName(name) {
 
 // app.js
 
-var kitin = angular.module('kitin', []);
+var kitin = angular.module('kitin', ['ui']);
 
 kitin.config(
   ['$locationProvider', '$routeProvider',
@@ -660,49 +660,6 @@ kitin.directive('keyEsc', function () {
     });
   }
 });
-
-/*kitin.directive('autoGrow', function() {
-return function(scope, element, attr){
-var minHeight = element[0].offsetHeight,
-paddingLeft = element.css('paddingLeft'),
-paddingRight = element.css('paddingRight');
- 
-var $shadow = angular.element('<div></div>').css({
-position: 'absolute',
-top: -10000,
-left: -10000,
-width: element[0].offsetWidth - parseInt(paddingLeft ? paddingLeft : 0) - parseInt(paddingRight ? paddingRight : 0),
-fontSize: element.css('fontSize'),
-fontFamily: element.css('fontFamily'),
-lineHeight: element.css('lineHeight'),
-resize: 'none'
-});
-angular.element(document.body).append($shadow);
- 
-var update = function() {
-var times = function(string, number) {
-    console.log("STRING: " + string + " Number: " + number);
-for (var i = 0, r = ''; i < number; i++) {
-r += string;
-}
-return r;
-}
- 
-var val = element.val().replace(/</g, '&lt;')
-.replace(/>/g, '&gt;')
-.replace(/&/g, '&amp;')
-.replace(/\n$/, '<br/>&nbsp;')
-.replace(/\n/g, '<br/>')
-.replace(/\s{2,}/g, function(space) { return times('&nbsp;', space.length - 1) + ' ' });
-$shadow.html(val);
- 
-element.css('height', Math.max($shadow[0].offsetHeight + 10, minHeight) + 'px');
-}
- 
-element.bind('keyup keydown keypress change', update);
-update();
-}
-});*/
 
 kitin.directive('fadable', function(conf) {
   return function(scope, elm, attrs) {
