dcbe483b150c68133f3c713911871a22b7becb4e
Niklas Lindström <lindstream@gmail.com>
2013-04-18 08:50:47 +0200

    Take screenshots during web tests

--- webtests/test_webapp.py
+++ webtests/test_webapp.py
@@ -1,8 +1,17 @@
 # -*- coding: UTF-8 -*-
+import os
 from time import sleep
 from nose.tools import with_setup
 from selenium.webdriver import Firefox
 from webdry import *
+
+
+screenshots = os.path.join(os.path.dirname(__file__), 'screenshots')
+
+def snap(name):
+    if not os.path.isdir(screenshots):
+        os.makedirs(screenshots)
+    save_screenshot(os.path.join(screenshots, name + '.png'))
 
 
 def setup():
@@ -40,14 +49,17 @@ def test_login_logout():
     get(env.start_page_url)
     do_login(u"test", u"test")
     assert find('.username').text.lower().startswith(u"test")
+    snap("Logged in")
     do_logout()
     with waits(0):
         assert find('.username') is None
+    snap("Logged out")
 
 @user_session()
 def test_searches():
     do_search("*")
     assert find('body.search') and find('section.result.record')
+    snap("Search Results")
     back()
     do_search("Tove")
     # goes directly to first result
@@ -58,11 +70,13 @@ def test_searches():
 def test_edit():
     get(env.start_page_url + "/edit/bib/7149593")
     sleep(2.0) # TODO: watch something appearing on init
+    snap("Edit Form")
     modinfo = find('section.modificationinfo')
     summary = find('*[data-ng-model="work.summary"]')
     summary.clear()
     summary.send_keys("...")
     assert 'saved' not in modinfo.get_attribute('class').split()
+    snap("Edit Modified")
     find('button#draft').click()
     for i in range(5):
         if 'saved' in modinfo.get_attribute('class').split():
@@ -85,5 +99,6 @@ def test_select_person():
     pname.send_keys(u"Tove")
     sleep(0.5)
     pname.send_keys(Keys.DOWN)
+    snap("Select Person")
     pname.send_keys(Keys.RETURN)
 
