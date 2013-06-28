e06738a3664c214e88ffea1b606635fd6f4ba967
Niklas Lindström <lindstream@gmail.com>
2013-06-28 13:36:07 +0200

    Improve fabfile.py (optional local fabconf.py, changed config.cfg generation)

--- fabfile.py
+++ fabfile.py
@@ -3,16 +3,23 @@
 import os, re
 from fabric.api import *
 from fabric.contrib.files import exists, upload_template
+try:
+    import fabconf as conf
+except ImportError:
+    pass
 
-#env.remotepath = ...
-#env.wwwuser = ...
-if 'wwwgroup' not in env and 'wwwuser' in env:
-    env.wwwgroup = env.wwwuser
-env.virtenvpath = '/var/virtualenvs/kitin'
+def _settings():
+    require('wwwuser', 'remotepath')
+    env.setdefault('virtenvpath', '/var/virtualenvs/kitin')
+    env.setdefault('wwwgroup', env.wwwuser)
+    env.setdefault('osflavour', 'ubuntu')
 
 @task
 def deploy():
-    require('wwwuser', 'wwwgroup', 'remotepath')
+    """
+    Deploy a new version of the Kitin web application
+    """
+    _settings()
     local("tar cfz /tmp/kitin.tgz --exclude='.*' --exclude=storage --exclude=config.cfg *")
     run('rm -f /tmp/kitin.tgz')
     put('/tmp/kitin.tgz', '/tmp/')
@@ -23,7 +30,6 @@ def deploy():
             sudo('tar xzf /tmp/kitin.tgz')
             sudo('ln -s ../storage .')
             sudo('ln -s ../config.cfg .')
-            #run('chown %(wwwuser)s storage' % env)
         upload_template("kitin.wsgi.in", "kitin.wsgi", env, backup=False)
         sudo("cp kitin.wsgi %(remotepath)s" % env)
         run("rm kitin.wsgi")
@@ -31,27 +37,25 @@ def deploy():
         run('pip install -r %(remotepath)s/requirements.txt' % env)
 
 @task
-def create_config(force=False):
-    if os.path.exists('config.cfg') and not int(force):
-        return
-    with open('config.cfg.in', 'r') as f:
-        file_contents = f.read()
-    with open('config.cfg', 'w') as newfile:
-        key = os.urandom(24).encode('hex')
-        newfile.write(_replace_setting_values(file_contents,
-            dict(os.environ, SESSION_SECRET_KEY=key)))
-
-def _replace_setting_values(text, data):
-    return re.sub('(\<)(\w+)(\>)',
-            lambda match: data.get(match.group(2), ''), text)
+def prepare():
+    """
+    Prepare the system for deployment (commonly done once per server)
+    """
+    _settings()
+    install_packages()
+    make_app_dirs()
+    create_config()
 
 @task
-def prepare():
-    # System requirements (tailored for Ubuntu Linux)
+def install_packages():
+    """Install system packages (Unbuntu Linux)"""
+    assert env.osflavour == 'ubuntu'
     sudo("apt-get install python-dev") # Python header files, for C-extensions
     sudo("apt-get install python-pip")
     sudo("apt-get install python-virtualenv") #or sudo("pip install virtualenv")
-    # Application directories
+
+@task
+def make_app_dirs():
     sudo("mkdir -p %(virtenvpath)s" % env)
     sudo('chown -R %(user)s %(virtenvpath)s' % env)
     run("virtualenv --distribute --never-download %(virtenvpath)s" % env)
@@ -59,6 +63,14 @@ def prepare():
     with cd(env.remotepath):
         if not exists("../storage"): sudo("mkdir ../storage")
         sudo('chown -R %(wwwuser)s:%(wwwgroup)s ..' % env)
+
+@task
+def create_config():
+    target_config = "%(remotepath)s/../config.cfg" % env
+    cfg = {}
+    execfile(os.path.join(os.path.dirname(__file__), 'config.cfg'), cfg)
+    cfg['SESSION_SECRET_KEY'] = os.urandom(24).encode('hex')
+    upload_template("config.cfg.in", target_config, cfg, use_sudo=True)
 
 @task
 def fetch_vendor_assets():
