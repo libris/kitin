ff8ce84d711053924747af518a1171b90c8a699d
sweco-semhul <martin.hultman@sweco.se>
2014-04-02 18:44:06 +0200

    Split controllers into multiple files. One per controller

---
+++ static/js/controllers/AppCtrl.js
@@ -0,0 +1,24 @@
+var kitin = angular.module('kitin.controllers', []);
+kitin.controller('AppCtrl', function($scope, $rootScope, $modal, searchService) {
+  $rootScope.state = {
+    searchType: {},
+    remoteDatabases: [],
+    search: {},
+
+    getSearchParams : function() {
+      var params = {
+        q: $rootScope.state.search.q,
+        start: $rootScope.state.search.page.start,
+        n: $rootScope.state.search.page.n,
+        sort: $rootScope.state.search.sort,
+        database: $rootScope.state.searchType.key === searchService.searchTypeIndex.remote.key ? $rootScope.state.search.database : undefined
+      };
+      if ($rootScope.state.search.f !== undefined) {
+        params.f = $rootScope.state.search.f;
+      }
+      return params;
+    }
+  };
+
+
+});
---
+++ static/js/controllers/ModalRemoteCtrl.js
@@ -0,0 +1,24 @@
+kitin.controller('ModalRemoteCtrl', function($scope, $rootScope, $modalInstance, definitions, searchService) {
+  // For remote search, load list of remote database definitions
+  if(_.isEmpty($rootScope.state.remoteDatabases)) {
+    definitions.remotedatabases.then(function(databases){
+      // Debug, set LC (Library of Congress) to default        
+      var searchedDatabases = ['LC'];
+      if($rootScope.state.search.database) {
+        searchedDatabases = $rootScope.state.search.database.split(',');
+      }
+      _.forEach(searchedDatabases, function(dbName) {
+        var i = _.findIndex(databases, { 'database': dbName });
+        if(i > 0) {
+          databases[i].selected = true;
+        }
+      });      
+
+      $rootScope.state.remoteDatabases = databases;
+    });
+  }
+
+  $scope.close = function() {
+    $modalInstance.close();
+  };
+});
