d69b08a54eedfbea2a1b07bc9925259383e04b99
sweco-semhul <martin.hultman@sweco.se>
2014-06-26 16:50:22 +0200

    Cleaned up promises in DataService

--- static/js/services/DataService.js
+++ static/js/services/DataService.js
@@ -4,112 +4,126 @@
  * Currently used for records and drafts
  */
 kitin.factory('dataService', function ($http, $q, editUtil, $rootScope) {
+
   return {
 
     record: {
+      /*  get
+      *   Get record from whelk API
+      *   If no record id is requested a new record is returned by type
+      *   @param    type    {String}    Type of record
+      *   @param    id      {String}    Record id (Optional)
+      */
       get: function (type, id) {
-        var record = $q.defer();
+        var deferer = $q.defer();
+        
         var path = '/record/template/' + type; // new record
         if(id) {
           path = $rootScope.API_PATH + '/' + type + '/' + id;
         }
-        var me = this;
+
         $http.get(path).success(function (struct, status, headers) {
-          record['recdata'] = editUtil.decorate(struct);
-          record['etag'] = headers('etag');
-          record.resolve(record);
+          deferer.resolve({
+            recdata: editUtil.decorate(struct),
+            etag: headers('etag')
+          });
         });
         
-        return record.promise;
+        return deferer.promise;
       },
 
-      save: function(type, id, data, etag) {
-        var record = $q.defer();
-        $http.put($rootScope.API_PATH + '/' + type + "/" + id, editUtil.undecorate(data),
-                  {headers: {"If-match":etag}}).success(function(data, status, headers) {
-          record['recdata'] = editUtil.decorate(data);
-          record['etag'] = headers('etag');
-          record.resolve(record);
-        }).error(function() {
-          console.log("FAILED to save record");
+      /*  save
+      *   
+      */
+      save: function(type, id, recordData, etag) {
+        var deferer = $q.defer();
+
+        $http.put($rootScope.API_PATH + '/' + type + "/" + id, editUtil.undecorate(recordData),
+            {
+              headers: {"If-match":etag}
+            })
+          .success(function(savedRecord, status, headers) {
+            deferer.resolve({
+              recdata: editUtil.decorate(savedRecord),
+              etag: headers('etag')  
+            });
         });
-        return record.promise;
+        return deferer.promise;
       },
 
       create: function(type, data) {
-        var record = $q.defer();
-        $http.post($rootScope.API_PATH, editUtil.undecorate(data)).success(function(data, status, headers) {
-          record['recdata'] = editUtil.decorate(data);
-          record['etag'] = headers('etag');
-          record.resolve(record);
+        var deferer = $q.defer();
+        $http.post($rootScope.API_PATH, editUtil.undecorate(data)).success(function(createdRecord, status, headers) {
+          deferer.resolve({
+              recdata: editUtil.decorate(createdRecord),
+              etag: headers('etag')  
+            });
         });
-        return record.promise;
+        return deferer.promise;
       },
 
       convertToMarc: function(data) {
-        var record = $q.defer();
+        var deferer = $q.defer();
         $http.post($rootScope.API_PATH + '/_format?to=application\/x-marc-json', editUtil.undecorate(data)).success(function(data, status, headers) {
-          record.resolve(data);
+          deferer.resolve(data);
         });
-        return record.promise;
+        return deferer.promise;
       }
     },
 
     draft: {
       get: function (draftId) {
-        var record = $q.defer();
+        var deferer = $q.defer();
         $http.get("/draft/" + draftId).success(function (data, status, headers) {
           data.document = editUtil.decorate(data.document);
-          record['recdata'] = data;
-          record['etag'] = headers('etag');
-          record.resolve(record);
+          deferer.resolve({
+            recdata: data,
+            etag: headers('etag')
+          });
         });
-        return record.promise;
+        return deferer.promise;
       },
 
       save: function(type, draftId, data, etag) {
-        var record = $q.defer();
+        var deferer = $q.defer();
         etag = etag ? etag : '';
         $http.put("/draft/" + type + '/' + draftId, editUtil.undecorate(data), {headers: {"If-match":etag } })
           .success(function(data, status, headers) {
-            record['recdata'] = editUtil.decorate(data);
-            record['etag'] = headers('etag');
-            record.resolve(record);
-            console.log("Saved record.");
-          })
-          .error(function() {
-            console.log("FAILED to save record");
+            deferer.resolve({
+              recdata: editUtil.decorate(data),
+              etag: headers('etag')
+            });
           });
-        return record.promise;
+        return deferer.promise;
       },
 
       create: function(type, data, etag) {
-        var record = $q.defer();
+        var deferer = $q.defer();
         etag = etag ? etag : '';
         $http.post("/draft/" + type, editUtil.undecorate(data), {headers: {"If-match":etag } })
           .success(function(data, status, headers) {
             data.document = editUtil.decorate(data.document); 
-            record.resolve(data);
+            deferer.resolve(data);
           });
-        return record.promise;
+        return deferer.promise;
       },
 
       delete: function(type, draftId) {
-        var record = $q.defer();
+        var deferer = $q.defer();
         $http.delete("/draft/" + type + '/' + draftId).success(function(data, status, headers) {
-          record.resolve(data);
+          deferer.resolve(data);
         });
-        return record.promise;
+        return deferer.promise;
       }
     },
 
     drafts: {
       get: function() {
-        var record = $q.defer();
+        var deferer = $q.defer();
         $http.get('/drafts').success(function(data, status, headers) {
-          record.resolve(data);
+          deferer.resolve(data);
         });
-        return record.promise;
+        return deferer.promise;
       }
     },
 
