945023ae6e5ea6b3fdc9ce0005e4aa43a0ea0cc2
Jakob Bergman <jakob@aino.se>
2014-10-08 14:16:40 +0200

    Start using skeletonmaps for holdings

--- static/js/controllers/ModalHoldingsCtrl.js
+++ static/js/controllers/ModalHoldingsCtrl.js
@@ -1,4 +1,4 @@
-kitin.controller('ModalHoldingsCtrl', function($scope, $rootScope, $modal, $modalInstance, $location, $http, recordId, editService, recordService, userData, definitions) {
+kitin.controller('ModalHoldingsCtrl', function($scope, $rootScope, $modal, $modalInstance, $location, $http, recordId, editService, recordService, userData) {
 
   $scope.recordId = recordId;
 
@@ -16,48 +16,30 @@ kitin.controller('ModalHoldingsCtrl', function($scope, $rootScope, $modal, $moda
     $modalInstance.close();
   };
 
-  console.log(definitions);
-
   // HOLDINGS
   recordService.holding.get(recordId, userData).then(function(response) {
     var data = response.data;
-    var items = data.list;
+    var holdings = data.list;
     var holdingEtags = {};
-    $scope.holdings = items;
-    var myHoldings = _.filter(items, function(i) {
-      var offers = i.data.about.offers;
-      for ( var j = 0; j < offers.length; j++ ) {
-        var heldBy = offers[j].heldBy;
-        for ( var k = 0; k < heldBy.length; k++ ) {
-          if ( heldBy[k].notation == userData.userSigel || heldBy[k].notation == 'KVIN' ) return true; // TODO: Don't compare to U, that's just for dev purposes
-        }
-      }
-    });
-    
-    if (myHoldings <= 0) {
+    $scope.holdings = holdings;
+
+    if (holdings <= 0) {
       console.log('No holdings found, creating new.\n');
       recordService.holding.create().then(function(response) {
-        var data = response.data;
+        var data = response;
         console.log(data);
-        data.data.about.holdingFor['@id'] = recordId;
-        data.data.about.offers[0].heldBy[0].notation = userData.userSigel;
-        $scope.holding = data;
+        data.data.holdingFor = {
+          '@id': recordId
+        };
+        data.data.offers[0].heldBy[0].notation = userData.userSigel;
         data._isNew = true; // TODO: don't do this when etag works
+        $scope.holding = data;
       });
     } else {
       console.log('Found', holdings.length, 'holdings, picking the first.\n', holdings);
       $scope.holding = holdings[0];
     }
-    // TODO: Remove this by end of week. 
-    // items.forEach(function (item) {
-    //   if (item.identifier) {
-    //     recordService.holding.getEtag(item.identifier).then(function(response) {
-    //       console.log(response);
-    //       holdingEtags[data['@id']] = response['etag'];
-    //     });
-    //   }
-    // });
-    // $scope.holdingEtags = holdingEtags;
+
   });
 
   $scope.addHolding = function(holdings) {
@@ -68,34 +50,20 @@ kitin.controller('ModalHoldingsCtrl', function($scope, $rootScope, $modal, $moda
     console.log('HOLDING: ', holding);
     recordService.holding.getEtag(holding['@id']).then(function(response) {
       var etag = response['etag'];
-      //holding['annotates'] = { '@id': '/' + recType + '/' + recordId };
-      // TODO: only use etag (but it's not present yet..)
-      if(!holding._isNew && (etag || holding.location === $scope.userSigel)) {
-        $http.put('/holding/' + holding['@id'].split('/').slice(-2)[1], holding, {headers: {'If-match':etag}}).success(function(data, status, headers) {
-          $scope.holdingEtags[data['@id']] = headers('etag');
-        }).error(function(data, status, headers) {
-          console.log('ohh crap!');
-        });
-      } else {
-        if (holding._isNew) { delete holding._isNew; }
-        console.log('we wants to post a new holding');
-        $http.post('/holding', holding).success(function(data, status, headers) {
-          $scope.holdingEtags[data['@id']] = headers('etag');
-        }).error(function(data, status, headers) {
-          console.log('ohh crap!');
-        });
-      }
+      if (holding._isNew) { delete holding._isNew; }
+      recordService.holding.save(holding, etag).then(function(response) {
+        console.log(response);
+      });
     });
   };
 
-  $scope.deleteHolding = function(holdingId) {
-    $http['delete']('/holding/' + holdingId).success(function(data, success) {
+  $scope.deleteHolding = function(holding) {
+    var holdingId = holding['@id']
+    recordService.holding.del(holdingId).then(function(response) {
       console.log('great success!');
-      $http.get('/record/' + recType + '/' + recordId + '/holdings').success(function(data) {
-        $scope.holdings = patchHoldings(data.list);
-      });
-    }).error(function() {
-      console.log('oh crap!');
+      // $http.get('/record/' + recType + '/' + recordId + '/holdings').success(function(data) {
+      //   $scope.holdings = patchHoldings(data.list);
+      // });
     });
   };
 
--- static/js/services/RecordService.js
+++ static/js/services/RecordService.js
@@ -2,7 +2,7 @@
  * recordService
  * Service to handle communcation with backend for records and drafts
  */
-kitin.factory('recordService', function ($http, $q, editService, $rootScope) {
+kitin.factory('recordService', function ($http, $q, editService, $rootScope, definitions) {
 
   return {
 
@@ -53,7 +53,7 @@ kitin.factory('recordService', function ($http, $q, editService, $rootScope) {
         var deferer = $q.defer();
         var recordDataCopy = angular.copy(recordData);
         editService.undecorate(recordDataCopy).then(function(undecoratedRecord) {
-          $http.put($rootScope.LOCAL_API_PATH + '/' + type + "/" + id, undecoratedRecord,
+          $http.put($rootScope.LOCAL_API_PATH + '/' + type + '/' + id, undecoratedRecord,
               {
                 headers: {"If-match":recordEtag}
               })
@@ -74,7 +74,7 @@ kitin.factory('recordService', function ($http, $q, editService, $rootScope) {
         var recordDataCopy = angular.copy(recordData);
         
         editService.undecorate(recordDataCopy).then(function(undecoratedRecord) {
-          $http.post($rootScope.LOCAL_API_PATH, undecoratedRecord)
+          $http.post($rootScope.LOCAL_API_PATH + '/' + type, undecoratedRecord)
             .success(function(createdRecord, status, headers) {
               editService.decorate(createdRecord).then(function(decoratedRecord) {
                 deferer.resolve({
@@ -171,9 +171,9 @@ kitin.factory('recordService', function ($http, $q, editService, $rootScope) {
       get: function(recordId, userData) {
         var deferer = $q.defer();
         var sigel = userData.userSigel;
-        sigel = 'KVIN'; // TODO: <--------------------------------  Haxxor shit, remove
+        //sigel = 'KVIN'; // TODO: <--------------------------------  Haxxor shit, remove
         $http.get($rootScope.API_PATH + '/hold/_search?q=*+about.holdingFor.@id:' + recordId.replace(/\//g, '\\/') + '+about.offers.heldBy.notation:' + sigel).success(function(data, status, headers) {
-          console.log(data);
+          console.log('DATA SHOULD LOOK LIKE THIS:', data.list);
           deferer.resolve({
             data: data,
             etag: headers('etag')
@@ -186,36 +186,70 @@ kitin.factory('recordService', function ($http, $q, editService, $rootScope) {
 
       create: function() {
         var deferer = $q.defer();
-        $http.get('/holding/bib/new').success(function(data, status, headers) {
-          console.log(data);
+        var recordSkeletonTypeMap = definitions.recordSkeletonTypeMap;
+        recordSkeletonTypeMap.then(function(skeletonTypeMap) {
+          console.log(skeletonTypeMap);
           deferer.resolve({
-            data: data,
-            etag: headers('etag')
+            data: skeletonTypeMap.main.HeldMaterial
           });
-        }).error(function(data, status, headers) {
-          console.log('RecordService failed creating holding.');
-        });
+        })
         return deferer.promise;
       },
 
       save: function(holding, etag) {
+        console.log(holding, etag);
         var deferer = $q.defer();
-        $http.put($rootScope.API_PATH + '/hold/' + holding['@id'].split('/').slice(-2)[1], holding, {headers: {'If-match':etag}}).success(function(data, status, headers) {
+        if (holding['@id'] && etag) {
+          $http.put($rootScope.API_PATH + holding['@id'], holding, {headers: {'Content-Type': 'application/ld+json', 'If-match':etag}}).success(function(data, status, headers) {
+            console.log(data, headers());
+            deferer.resolve({
+              data: data,
+              etag: headers('etag')
+            });
+          }).error(function(data, status, headers) {
+            console.log('RecordService failed saving holding.');
+          });
+        } else {
+          // Holding has no ID, assume it's new
+          $http.post($rootScope.API_PATH + '/hold', holding, {headers: {'Content-Type': 'application/ld+json'}}).success(function(data, status, headers) {
+            console.log('DATA:', data, 'STATUS:', status, 'HEADERS:', headers());
+            var identifier = headers('location');
+            if (identifier) {
+              identifier = '/hold/' + identifier.split('/').slice(-2)[1];
+              data['@id'] = data.about['@id'] = identifier;
+            }
+            deferer.resolve({
+              data: data,
+              etag: headers('etag')
+            });
+          }).error(function(data, status, headers) {
+            console.log('RecordService failed saving new holding.');
+          });
+        }
+        return deferer.promise;
+      },
+
+      del: function(holdingId) {
+        var deferer = $q.defer();
+        $http['delete']($rootScope.API_PATH + holdingId).success(function(data, success, headers) {
+          console.log(data, success, headers);
           deferer.resolve({
             data: data,
             etag: headers('etag')
           });
         }).error(function(data, status, headers) {
-          console.log('RecordService failed saving holding.');
+          console.log('RecordService failed deleting holding.');
         });
+        return deferer.promise;
       },
 
       getEtag: function(holdingId) {
         console.log(holdingId);
         var deferer = $q.defer();
-        $http.get($rootScope.API_PATH + '/hold/'+ itemId.split('/').slice(-2)[1]).success(function(data, status, headers) {
+          $http.get($rootScope.API_PATH + holdingId).success(function(data, status, headers) {
+            var etag = headers('etag') ? headers('etag') : null;
           deferer.resolve({
-            etag: headers('etag')
+            etag: etag
           });
         }).error(function(data, status, headers) {
           console.log('RecordService failed getting eTag for holding.');
