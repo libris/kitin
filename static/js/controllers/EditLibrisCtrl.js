ce54c8eb4e3cb4ae7b80b33ee9333a0f3955406c
semhul <martin.hultman@sweco.se>
2014-10-20 13:41:43 +0200

    Fixed MARC conversion

--- static/js/controllers/EditLibrisCtrl.js
+++ static/js/controllers/EditLibrisCtrl.js
@@ -8,18 +8,19 @@ kitin.controller('EditLibrisCtrl', function($scope, $controller, $location, reco
     // !TODO create a cleaner way to detect data format and sperate from bib
     if($location.$$path.indexOf('/marc/') !== -1) {
       delete record.about.publicationCountry;
-      recordService.record.convertToMarc(record).then(function(data) {
+      recordService.libris.convertToMarc(record).then(function(data) {
         $scope.record = data;
       });
-    }
+    } else {
 
-    $scope.record = record;
-    $scope.etag = data['etag'];
-    
+      $scope.record = record;
+      $scope.etag = data['etag'];
+      
 
-    // BIB
-    if ($scope.recType === editService.RECORD_TYPES.BIB) {
-      $scope.addRecordViewsToScope(record, $scope);
+      // BIB
+      if ($scope.recType === editService.RECORD_TYPES.BIB) {
+        $scope.addRecordViewsToScope(record, $scope);
+      }
     }
   });
 });
