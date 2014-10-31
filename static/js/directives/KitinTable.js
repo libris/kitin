82094bfc2501ba6d9a98cf74d532fec517599e11
semhul <martin.hultman@sweco.se>
2014-10-31 12:58:56 +0100

    Fixed kitin-table add and remove functionality

--- static/js/directives/KitinTable.js
+++ static/js/directives/KitinTable.js
@@ -13,24 +13,41 @@ kitin.directive('kitinTable', function(editService){
       template: '<div class="label" ng-hide="isEmpty(model) && options.hidden">' + 
                   '<span class="lbl">{{title | translate}}</span>' +
                   '<span class="inp">' +
-                    '<table ng-init="m = createFirstRowIfEmpty(model)">' +
+                    '<table ng-init="model = createFirstRowIfEmpty(model)">' +
                       '<thead>' +
                         '<tr class="thead" ng-if="titles.length">' +
                           '<th ng-repeat="title in titles">{{title}}</th>' +
                         '</tr>' +
                       '</thead>' +
                       '<tbody>' +
-                        '<tr kitin-tr-controls ng-repeat="item in m track by $index">' +
+                        '<tr kitin-tr-controls ng-transclude ng-repeat="item in model track by $index">' +
                         '</tr>' +
                       '</tbody>' +
                     '</table>' +
+                  '</span>' + 
+                  '<span>' +
+                    '<a href="" ng-click="addRow()">Lägg till fält</a>' +
+                  '</span>' +
                 '</div>',
       controller: function($element, $scope, $attrs, $rootScope) {
+        
         $scope.isEmpty = $rootScope.isEmpty;
         $scope.title = 'LABEL.' + $attrs.model;
         $scope.titles = $scope.$eval($attrs.titles);
+
+        // TODO! Create object for each model. Use editService.createObject?
+        var createObject = function() { return {}; };
+
         $scope.createFirstRowIfEmpty = function(model) {
-          return _.isArray(model) && model.length > 0 ? model : [{}];
+          return _.isArray(model) && model.length > 0 ? model : [createObject()]; 
+        };
+
+        $scope.addRow = function(index) {
+          return $scope.model.push(createObject());
+        };
+
+        $scope.removeRow = function(index) {
+          return $scope.model.splice(index,1);
         };
 
       }
@@ -38,23 +55,22 @@ kitin.directive('kitinTable', function(editService){
 });
 
 
-kitin.directive('kitinTrControls', [function() {
+kitin.directive('kitinTrControls', ['$compile', function($compile) {
   return {
     // Type element isnt working since the transclude function is manipulating the element after its been added to the DOM
     // and a table needs to have correct <tr>-tags to render propperly
     restrict: 'AC',
-    replace: true,
     link: function(scope, element, attrs, controller, transcludeFn) {
-
-      transcludeFn(function(clone) {
+      transcludeFn(scope,function(clone) {
         element.empty();
         // Append button to each row
-        element.append(clone).append(
+        var controls = angular.element(
           '<td>' + 
-            '<button class="btn-link deleter" data-ng-click="$parent.removeTableRow($index)">' +
-              '<i class="fa fa-times"></i>' +
-            '</button>' + 
-          '<td>');
+            '<button class=btn-link deleter" data-ng-click="removeRow($index)"><i class="fa fa-times"></i></button>' + 
+          '</td>');
+        element.append(clone).append(controls);
+        // Controls needs compiling to be bound to scope
+        $compile(controls)(scope);
       });
     }
   };
