f537b5ce43f483d367e989d526e2305fd2080ae8
semhul <martin.hultman@sweco.se>
2015-01-08 14:17:52 +0100

    Updating vendor files

--- static/vendor/angular-animate/angular-animate.js
+++ static/vendor/angular-animate/angular-animate.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.29-build.581+sha.b79f583
+ * @license AngularJS v1.2.29-build.588+sha.3cbf542
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
