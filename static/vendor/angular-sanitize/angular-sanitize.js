ed24e3e88a22ca57c816b315bf8b6fed06793ba6
semhul <martin.hultman@sweco.se>
2015-01-27 11:07:51 +0100

    Vendor update

--- static/vendor/angular-sanitize/angular-sanitize.js
+++ static/vendor/angular-sanitize/angular-sanitize.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.4.0-build.3803+sha.331cac2
+ * @license AngularJS v1.4.0-build.3807+sha.b3a9bd3
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
