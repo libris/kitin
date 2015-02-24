5ea4ab74357b15c5507b291e7dc871e7c4c8e399
Ola Blissing <ola.blissing@gmail.com>
2015-02-24 17:14:42 +0100

    Handle totalResults in remote Fixes #198

--- static/js/controllers/SearchResultCtrl.js
+++ static/js/controllers/SearchResultCtrl.js
@@ -182,8 +182,19 @@ kitin.controller('SearchResultCtrl', function($scope, $http, $timeout, $location
         }
 
         var pageSize = $rootScope.state.getSearchParams().n;
-        $rootScope.state.search.hitCount = data.totalResults;
-        $rootScope.state.search.page.total = Math.ceil(data.totalResults / pageSize);
+
+        // Remote results come as an object, libris results as an int
+        if(typeof data.totalResults === 'object') {
+          $rootScope.state.search.hitCount = 0;
+          var tmp = _.values(data.totalResults);
+          for (var i = 0; i < tmp.length;i++){
+            $rootScope.state.search.hitCount += tmp[i];
+          }
+        } else {
+          $rootScope.state.search.hitCount = data.totalResults;
+        }
+
+        $rootScope.state.search.page.total = Math.ceil($rootScope.state.search.hitCount / pageSize);
         // Everything we need is set, change paginator page
         var page = ($rootScope.state.search.page.start / pageSize || 0) + 1;
         // var page = ($rootScope.state.search.page.start / $rootScope.state.search.page.n || 0) + 1;
@@ -195,7 +206,6 @@ kitin.controller('SearchResultCtrl', function($scope, $http, $timeout, $location
       $scope.staticQ = angular.copy($rootScope.state.search.q);
     });
   };
-
 
   // TODO: Put this in better place for access from both result list and bib modal.
   $scope.importRecord = function(data) {
