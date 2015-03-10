542ee2984af9ab8edd3ab1db77f9c274f5e665d1
semhul <martin.hultman@sweco.se>
2015-03-10 15:49:29 +0100

    Fixed help lookup bug in kitin help

--- static/js/directives/KitinHelp.js
+++ static/js/directives/KitinHelp.js
@@ -43,12 +43,13 @@ kitin.directive('kitinHelp', function (definitions) {
               var modelParts = model.split('.');
               var lastModel = modelParts[modelParts.length-1];
               var comment = terms.getComment(lastModel);
-              if(comment && comment !== lastModel || comment !== '') {
+              if(comment && comment !== lastModel && comment !== '') {
                 $scope.helpText = comment;
               } else {
                 // Try to get helptext from labels json
-                var translatedHelpText = $translate.instant(model);
-                if(translatedHelpText !== model) {
+                var helpModel = model.replace('LABEL','HELP');
+                var translatedHelpText = $translate.instant(helpModel);
+                if(translatedHelpText !== helpModel) {
                   $scope.helpText = translatedHelpText;
                 }
               }
