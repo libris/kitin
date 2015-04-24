5304a72559ea0a4375a4fc0424cc1b128bd583f4
semhul <martin.hultman@sweco.se>
2015-04-24 09:14:44 +0200

    Added user configuration to fix problem with karma tests

--- test/js/config/globals.js
+++ test/js/config/globals.js
@@ -3,3 +3,8 @@ var API_PATH = WHELK_HOST = 'http://localhost:8180/whelk';
 var WHELK_WRITE_HOST = 'http://localhost:5000/whelk';
 var TESTING_SKIP_TRANSLATION = 'DO NOT TRANSLATE';
 var MAIN_STATUS_MSG = {};
+var CURRENT_USER = {
+  sigel: 'SEK',
+  username: 'App tests user',
+  authorization: [{ 'sigel': 'NONE', 'xlreg': true, 'kat': true, 'reg': true }]
+};
